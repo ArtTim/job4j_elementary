7b4b59d7189951b2da33d0127b97deb71c418504
ArtTim <tim-murr@inbox.ru>
2023-07-30 16:18:55 +0300

    4.3.5. Множественное логическое выражение И [#9461 #433229]

--- src/main/java/ru/job4j/condition/Triangle.java
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -2,7 +2,6 @@ package ru.job4j.condition;
 
 public class Triangle {
     public static boolean exist(double ab, double ac, double bc) {
-        // Нужно проверить, что ab + ac > bc и ac + bc > ab и ab + bc > ac - в этом случае треугольник существует.
         return ab + ac > bc && ac + bc > ab && ab + bc > ac;
     }
 }
