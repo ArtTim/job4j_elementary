7008c5c7b8554eb591e29c312fee0b9bacb5c5bc
ArtTim <tim-murr@inbox.ru>
2024-08-18 21:56:30 +0300

    6. Идеальный вес [#41584 #432662]

---
+++ src/main/java/ru/job4j/Fit.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+/*
+Идеальный вес для мужчин = (рост в сантиметрах – 100) · 1,15.
+Идеальный вес для женщин = (рост в сантиметрах – 110) · 1,15.
+ */
+
+public class Fit {
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(short height) {
+        return (height - 110) * 1.15;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        System.out.println("Man 187 is " + manWeight(height));
+        System.out.println("Man 187 is " + womanWeight(height));
+    }
+}
