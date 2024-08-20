ff012f044fbba87f9dfdba88a17f7fac528b4b68
ArtTim <tim-murr@inbox.ru>
2024-08-20 20:47:24 +0300

    4. Тесты для стороны прямоугольника. [#159460 #433147]

--- src/main/java/ru/job4j/RectangleArea.java
+++ src/main/java/ru/job4j/RectangleArea.java
@@ -2,12 +2,6 @@ package ru.job4j;
 
 public class RectangleArea {
 
-    /*
-    p = 2 * (l + h); где l - длина, h - высота.
-    h = p / (2 * (k + 1))
-    l = h * k;
-    s = l * h;
-     */
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
@@ -17,5 +11,6 @@ public class RectangleArea {
     public static void main(String[] args) {
         System.out.println(square(6, 2)); // 2
         System.out.println(square(4, 1)); // 1
+        System.out.println(square(5, 1)); // 1
     }
 }
---
+++ src/test/java/ru/job4j/RectangleAreaTest.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class RectangleAreaTest {
+
+    @Test
+    void whenP2K2Then2() {
+        double expected = 2;
+        double output = RectangleArea.square(6, 2);
+        assertThat(output).isEqualTo(expected, withPrecision(0.0001));
+    }
+
+    @Test
+    void whenP4k1Then1() {
+        double expected = 1;
+        double output = RectangleArea.square(4, 1);
+        assertThat(output).isEqualTo(expected, withPrecision(0.0001));
+    }
+
+    @Test
+    void whenP5K1Then1Dot5625() {
+        double expected = 1.5625;
+        double output = RectangleArea.square(5, 1);
+        assertThat(output).isEqualTo(expected, withPrecision(0.0001));
+    }
+}
