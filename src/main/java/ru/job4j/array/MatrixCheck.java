72467a830626603b524bda55f7cb948c8fa6f843
ArtTim <tim-murr@inbox.ru>
2024-08-24 20:30:09 +0300

    Нужно будет перезалить решение.

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -31,12 +31,4 @@ public class MatrixCheck {
         return array;
     }
 
-    public static boolean isWin(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 'X' && (monoVertical(board, i) || monoHorizontal(board, i))) {
-                    return true;
-            }
-        }
-        return false;
-    }
 }
