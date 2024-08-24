1a041b598c743d1d82e6d9a8faaeb019b9b1ce96
ArtTim <tim-murr@inbox.ru>
2024-08-24 23:03:57 +0300

    Нужно будет перезалить эти файлы.

--- src/main/java/ru/job4j/condition/Fit.java
+++ src/main/java/ru/job4j/condition/Fit.java
@@ -1,21 +1,17 @@
 package ru.job4j.condition;
 
 public class Fit {
-    public static double calculateWeight(short height, int baseHeight) {
-        return (height - baseHeight) * 1.15;
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
     }
 
-    public static double calculateManWeight(short height) {
-        return calculateWeight(height, 100);
-    }
-
-    public static double calculateWomanWeight(short height) {
-        return calculateWeight(height, 110);
+    public static double womanWeight(short height) {
+        return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
         short height = 177;
-        System.out.printf("Man with height %s cm has ideal weight: %.2f kg %s", height, calculateManWeight(height), System.lineSeparator());
-        System.out.printf("Woman with height %s cm has ideal weight %.2f kg %s", height, calculateWomanWeight(height), System.lineSeparator());
+        System.out.printf("Man %s is " + manWeight(height) + System.lineSeparator(), height);
+        System.out.printf("Man %s is " + womanWeight(height) + System.lineSeparator(), height);
     }
 }
